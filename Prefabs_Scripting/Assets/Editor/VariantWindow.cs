e3bd597db89cd817975a70868d5532fb080a69f9
Sophia Clarke <sophiaclarke93@gmail.com>
2018-06-21 12:18:23 +0100

    Adding code comments to explain some of the API calls

--- Prefabs_Scripting/Assets/Editor/VariantWindow.cs
+++ Prefabs_Scripting/Assets/Editor/VariantWindow.cs
@@ -17,7 +17,7 @@ public class VariantWindow : EditorWindow {
 		EditorGUILayout.LabelField ("View all prefab variants", EditorStyles.boldLabel);
 		EditorGUILayout.Space();
 
-
+		// This is the field where the prefab can be dragged into
 		parent = (GameObject)EditorGUILayout.ObjectField("Parent Prefab", parent, typeof(GameObject));
 
 		EditorGUILayout.Space();
@@ -25,13 +25,16 @@ public class VariantWindow : EditorWindow {
 
 		List<Object> variants = GetVariants();
 
+		// The group is disabled because we want these ObjectFields to be read-only
 		EditorGUI.BeginDisabledGroup(true);
 		foreach (Object variant in variants)
 		{
 			EditorGUILayout.ObjectField("Variant:", variant, typeof(Object), false);
+
+			// Show a thumbnail of the variant prefabs
 			Texture2D image = AssetPreview.GetAssetPreview(variant);
-			//image.Resize(100, 100);
 			
+			// The thumbnail image is inside a LabelField because DrawPreviewTexture isn't available in EditorGUILayout
 			EditorGUILayout.LabelField(new GUIContent("Preview:"),new GUIContent(image), GUILayout.Height(100.0f), GUILayout.Width(500.0f));
 			EditorGUILayout.Space();
 		}
@@ -41,16 +44,21 @@ public class VariantWindow : EditorWindow {
 
 	List<Object> GetVariants()
 	{
+		// Find all prefabs
 		string[] guids = AssetDatabase.FindAssets("t:Prefab");
 
 		List<Object> variants = new List<Object>();
 
 		foreach (string guid in guids)
 		{
+			// Convert the prefab guid string into Object
 			string path = AssetDatabase.GUIDToAssetPath(guid);
         	Object obj = AssetDatabase.LoadAssetAtPath<Object>(path);
+
+			// Check if the prefab is a variant
 			if (PrefabUtility.GetPrefabAssetType(obj) == PrefabAssetType.Variant)
 			{
+				// Check that the base prefab is the same as the one we've dragged into the ObjectField (this used to be PrefabUtility.GetPrefabParent)
 				if (PrefabUtility.GetCorrespondingObjectFromSource(obj) == parent)
 				{
 					variants.Add(obj);
